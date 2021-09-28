b01b3a60e24e388ecb2aab8bb541c1b48b1ca0f9
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2021-09-28 11:28:24 +0900

    Update Main.java

--- [2098번] 외판원 순회/Main.java
+++ [2098번] 외판원 순회/Main.java
@@ -1,49 +1,37 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-
+import java.util.*;
+public class Main{
     static int N;
-    static int[][] w, dp;
-    static final int INF = 16000000;
-
-    public static void main(String[] args) throws Exception {
-
+    static int[][] w;
+    static int[][] dp;
+    static int INF;
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        INF = 10000000;
         N = sc.nextInt();
-        dp = new int[N][(1 << N) - 1];
-        w = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                w[i][j] = sc.nextInt();
+        w= new int[N][N];
+        dp = new int[N][(1<<N)];
+        for(int i=0;i<N;i++){
+            Arrays.fill(dp[i],INF);
+            for(int j=0;j<N;j++){
+                w[i][j]= sc.nextInt();
             }
         }
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dp[i], INF);
-        }
-
         System.out.println(dfs(0,1));
     }
-
-    public static int dfs(int node, int visit) {
-
-        if (visit == (1 << N) - 1) { // base case
-            if (w[node][0] == 0)
+    public static int dfs(int node,int visit){
+        if(visit == (1<<N)-1){   //base
+            if(w[node][0]==0){
                 return INF;
+            }
             return w[node][0];
         }
+        if(dp[node][visit]!=INF) return dp[node][visit];
 
-        if (dp[node][visit] != INF) {
-            return dp[node][visit];
-        }
-
-        for (int i = 0; i < N; i++) {
-            if ((visit & (1 << i)) == 0 && w[node][i] != 0) {
-                dp[node][visit] = Math.min(dp[node][visit], dfs(i, visit | (1 << i)) + w[node][i]);
+        for(int i=0;i<N;i++){
+            if(w[node][i]!=0 && (visit & (1<<i))==0 ) {
+                dp[node][visit] = Math.min(dp[node][visit], dfs(i, (visit | (1 << i))) + w[node][i]);
             }
         }
         return dp[node][visit];
     }
-
 }
